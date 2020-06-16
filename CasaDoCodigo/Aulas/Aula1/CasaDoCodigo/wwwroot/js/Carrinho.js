0b0db4cc900a59d4f72a0698bf33259590373075
Monique Ribeiro Sousa <ribeiromonique90@gmail.com>
2020-06-16 16:45:35 -0300

    adiciona ValidateAntiForgeryToken nas requisições para proteção contra CSRF

--- CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/wwwroot/js/Carrinho.js
+++ CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/wwwroot/js/Carrinho.js
@@ -29,11 +29,17 @@
     }
 
     postQuantidade(data) {
+
+        let token = $('[name=__RequestVerificationToken]').val();
+        let headers = {};
+        headers['RequestVerificationToken'] = token;
+
         $.ajax({
             url: "/pedido/updatequantidade",
             type: "POST",
             contentType: "application/json",
-            data: JSON.stringify(data)
+            data: JSON.stringify(data),
+            headers: headers
         }).done(function (response) {
             let itemPedido = response.itemPedido;
             let linhaDoItem = $('[item-id=' + itemPedido.id + ']');
@@ -48,7 +54,6 @@
                 linhaDoItem.remove();
             }
 
-            debugger;
         });
     }
 
